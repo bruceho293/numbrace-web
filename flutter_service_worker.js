46055f0fd8dcf90259cc28eaffb39489613cdc29
Huan Ho (Bruce) <37372806+bruceho293@users.noreply.github.com>
2026-09-24 17:16:52 +0700

    Game improvement

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -56,14 +56,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "606078722a8a87f40fe70b03f5630cd8
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "9424179562d6622894963f1bfb83033a",
+"flutter_bootstrap.js": "d550e9c90f33898c4495d8291903cddb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "2fe90a60d5fcb519a09f82189ced46c4",
 "/": "2fe90a60d5fcb519a09f82189ced46c4",
-"main.dart.js": "38854767a7059d661389cafd0f29f786",
+"main.dart.js": "18a5b76792aad65709db0953fc5d3bdf",
 "manifest.json": "8129b6fffadb073c0e531854ea5bda46",
 "version.json": "a0f6269aa53c47e571bf53266b7df660"};
 // The application shell files that are downloaded before a service worker can
